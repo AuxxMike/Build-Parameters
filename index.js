b3c75710b2465b498414e99446fb817238a93922
Michael Lersbak <michaell@stcu.org>
2019-12-20 12:15:42 -0800

    fresh index.js

--- index.js
+++ index.js
@@ -2,69 +2,71 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 core.setOutput("version", "76.76.76");
+const eventPayload = JSON.stringify(github.context.payload, undefined, 2);
+core.exportVariable('mikeVersion', 'auxx76');
+console.log('The event payload: ${eventPayload}');
 
-core.run(async tools =>{
-    const pkg = tools.getPackageJSON();
-    console.log('The pkg: ${pkg}');
-    const event = tools.context.payload
-    console.log('The event : ${event}');
-    const eventPayload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log('The event payload: ${eventPayload}');
+// core.run(async tools =>{
+//     const pkg = tools.getPackageJSON();
+//     console.log('The pkg: ${pkg}');
+//     const event = tools.context.payload
+//     console.log('The event : ${event}');
 
-    const messages = event.commits.map(commit => commit.message + '\n' + commit.body);
 
-    const commitMessage = 'version bump to';
-    const isVersionBump = messages.map(message => message.toLowerCase().includes(commitMessage)).includes(true);
-    if (isVersionBump){
-        tools.exit.success('No action necessary!')
-        return
-    }
+//     const messages = event.commits.map(commit => commit.message + '\n' + commit.body);
 
-    let version = 'patch'
-    if (messages.map(message => message.includes('MAJORVERSION')).includes(true)){
-        version = 'major'
-    } else if (messages.map(message => message.toLowerCase().startsWith('feat')).includes(true)) {
-        version = 'minor'
-    }
+//     const commitMessage = 'version bump to';
+//     const isVersionBump = messages.map(message => message.toLowerCase().includes(commitMessage)).includes(true);
+//     if (isVersionBump){
+//         tools.exit.success('No action necessary!')
+//         return
+//     }
 
-    try{
-        const projectName = core.getInput('project-name');
-        const currentVersion = core.getInput('current-version');
+//     let version = 'patch'
+//     if (messages.map(message => message.includes('MAJORVERSION')).includes(true)){
+//         version = 'major'
+//     } else if (messages.map(message => message.toLowerCase().startsWith('feat')).includes(true)) {
+//         version = 'minor'
+//     }
 
-        const current = pkg.version.toString()
-        // set git user
-        //await tools.runInWorkspace('git', ['config', 'user.name', '"Automated Version Bump"'])
-        //await tools.runInWorkspace('git', ['config', 'user.email', '"gh-action-bump-version@users.noreply.github.com"'])
+//     try{
+//         const projectName = core.getInput('project-name');
+//         const currentVersion = core.getInput('current-version');
+
+//         const current = pkg.version.toString()
+//         // set git user
+//         //await tools.runInWorkspace('git', ['config', 'user.name', '"Automated Version Bump"'])
+//         //await tools.runInWorkspace('git', ['config', 'user.email', '"gh-action-bump-version@users.noreply.github.com"'])
     
-        const currentBranch = /refs\/[a-zA-Z]+\/(.*)/.exec(process.env.GITHUB_REF)[1]
-        console.log('currentBranch:', currentBranch)
+//         const currentBranch = /refs\/[a-zA-Z]+\/(.*)/.exec(process.env.GITHUB_REF)[1]
+//         console.log('currentBranch:', currentBranch)
     
-        // do it in the current checked out github branch (DETACHED HEAD)
-        // important for further usage of the package.json version
-        await tools.runInWorkspace('npm',
-          ['version', '--allow-same-version=true', '--git-tag-version=false', current])
-        console.log('current:', current, '/', 'version:', version)
-        let newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString().trim()
-        await tools.runInWorkspace('git', ['commit', '-a', '-m', `"ci: ${commitMessage} ${newVersion}"`])
+//         // do it in the current checked out github branch (DETACHED HEAD)
+//         // important for further usage of the package.json version
+//         await tools.runInWorkspace('npm',
+//           ['version', '--allow-same-version=true', '--git-tag-version=false', current])
+//         console.log('current:', current, '/', 'version:', version)
+//         let newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString().trim()
+//         await tools.runInWorkspace('git', ['commit', '-a', '-m', `"ci: ${commitMessage} ${newVersion}"`])
     
-        // now go to the actual branch to perform the same versioning
-        await tools.runInWorkspace('git', ['checkout', currentBranch])
-        await tools.runInWorkspace('npm',
-          ['version', '--allow-same-version=true', '--git-tag-version=false', current])
-        console.log('current:', current, '/', 'version:', version)
-        newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString().trim()
-        newVersion = `${process.env['INPUT_TAG-PREFIX']}${newVersion}`
-        console.log('new version:', newVersion)
-        await tools.runInWorkspace('git', ['commit', '-a', '-m', `"ci: ${commitMessage} ${newVersion}"`])
+//         // now go to the actual branch to perform the same versioning
+//         await tools.runInWorkspace('git', ['checkout', currentBranch])
+//         await tools.runInWorkspace('npm',
+//           ['version', '--allow-same-version=true', '--git-tag-version=false', current])
+//         console.log('current:', current, '/', 'version:', version)
+//         newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString().trim()
+//         newVersion = `${process.env['INPUT_TAG-PREFIX']}${newVersion}`
+//         console.log('new version:', newVersion)
+//         await tools.runInWorkspace('git', ['commit', '-a', '-m', `"ci: ${commitMessage} ${newVersion}"`])
     
-        const remoteRepo = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`
-        // console.log(Buffer.from(remoteRepo).toString('base64'))
-        await tools.runInWorkspace('git', ['tag', newVersion])
-        await tools.runInWorkspace('git', ['push', remoteRepo, '--follow-tags'])
-        await tools.runInWorkspace('git', ['push', remoteRepo, '--tags'])
-      } catch (e) {
-        tools.log.fatal(e)
-        tools.exit.failure('Failed to bump version...')
-      }
-      tools.exit.success('Version bumped!')
-})
+//         const remoteRepo = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`
+//         // console.log(Buffer.from(remoteRepo).toString('base64'))
+//         await tools.runInWorkspace('git', ['tag', newVersion])
+//         await tools.runInWorkspace('git', ['push', remoteRepo, '--follow-tags'])
+//         await tools.runInWorkspace('git', ['push', remoteRepo, '--tags'])
+//       } catch (e) {
+//         tools.log.fatal(e)
+//         tools.exit.failure('Failed to bump version...')
+//       }
+//       tools.exit.success('Version bumped!')
+//})
